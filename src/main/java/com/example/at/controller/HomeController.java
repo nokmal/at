868e684b30ea0a04038b5cdb197f6b1549bcf951
nokmal <9aedarae@gmail.com>
2021-01-05 13:30:35 +0900

    HomeController Test

---
+++ src/main/java/com/example/at/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.at.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕";
+	}
+
+	@RequestMapping("/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "안녕2";
+	}
+
+	@RequestMapping("/home/plus")
+	@ResponseBody
+	public int showPlus(int a, int b) {
+		return a + b;
+	}
+
+	@RequestMapping("/home/increase")
+	@ResponseBody
+	public int showIncrease() {
+		return 1;
+	}
+}
