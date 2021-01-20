ea63d09bee78096174fec1ce8b9238ee352bbf73
nokmal <9aedarae@gmail.com>
2021-01-20 21:47:20 +0900

    update dp.sql

--- db.sql
+++ db.sql
@@ -1,40 +1,28 @@
-# DB 세팅
-DROP DATABASE IF EXISTS `at`;
-CREATE DATABASE `at`;
-USE `at`;
+FROM article;
 
-# article 테이블 세팅
-CREATE TABLE article (
+# member 테이블 세팅
+CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME,
     updateDate DATETIME,
     delDate DATETIME,
 	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-	displayStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-    title CHAR(200) NOT NULL,
-    `body` LONGTEXT NOT NULL
+	authStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    loginId CHAR(20) NOT NULL UNIQUE,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    `nickname` CHAR(20) NOT NULL,
+    `email` CHAR(100) NOT NULL,
+    `phoneNo` CHAR(20) NOT NULL
 );
 
-# article 테이블에 테스트 데이터 삽입
-INSERT INTO article
+# member 테이블에 테스트 데이터 삽입
+INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
-title = '제목1',
-`body` = '내용1';
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목2',
-`body` = '내용2',
-displayStatus = 1;
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목3',
-`body` = '내용3',
-displayStatus = 1;
-
-SELECT *
-FROM article; 
+loginId = 'admin',
+loginPw = SHA2('admin', 256),
+`name` = '관리자',
+`nickname` = '관리자',
+`email` = '',
+`phoneNo` = ''; 
