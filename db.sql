56f56716a2740394322b9f20eebe4f6ef65eefa3
nokmal <9aedarae@gmail.com>
2021-01-22 11:12:39 +0900

    update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,36 @@
-FROM article;
+# DB 세팅
+DROP DATABASE IF EXISTS `at`;
+CREATE DATABASE `at`;
+USE `at`;
+# article 테이블 세팅
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME,
+    updateDate DATETIME,
+    delDate DATETIME,
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+	displayStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    title CHAR(200) NOT NULL,
+    `body` LONGTEXT NOT NULL
+);
+# article 테이블에 테스트 데이터 삽입
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2',
+displayStatus = 1;
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+displayStatus = 1;
 
 # member 테이블 세팅
 CREATE TABLE `member` (
@@ -15,7 +47,6 @@ CREATE TABLE `member` (
     `email` CHAR(100) NOT NULL,
     `phoneNo` CHAR(20) NOT NULL
 );
-
 # member 테이블에 테스트 데이터 삽입
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -25,4 +56,4 @@ loginPw = SHA2('admin', 256),
 `name` = '관리자',
 `nickname` = '관리자',
 `email` = '',
-`phoneNo` = ''; 
+`phoneNo` = '';
